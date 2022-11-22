21f41e2db1694edae85ef8e5508c66c14aabd2ef
Bryan140422 <bryanymbaubedari@gmail.com>
2022-11-22 17:05:02 +0700

    Hacktheraphy Changes to be committed: 	new file:   pages/respon_komplain/index.tsx

---
+++ pages/respon_komplain/index.tsx
@@ -0,0 +1,232 @@
+export default function ResponKomplian(){
+    return(
+        <>
+           <div className="body">
+            <div className="flex flex-nowrap">
+
+            <div className="flex-none p-4 m-2">
+            <div className="relative">
+                
+                <div className="gap">
+                ‎ 
+                ‎ 
+                ‎ 
+                </div>
+
+                
+            </div>
+            </div>
+
+            <div className="block rounded-lg shadow-lg bg-gray-400 w-1/2 h-auto text-left mb-10">
+            <div className="flex-none">
+            <div className="Submission1">
+                <div className="mb-3 w-full inline-flex">
+                    <label htmlFor="exampleText0" className="form-label inline-block mb-2 text-white ml-2 mt-5"
+                    >Judul:</label
+                    >
+                    <div className="w-full  ml-14 mr-4 mt-4">
+                   
+                    <input
+                    type="text"
+                    className="
+                        form-control
+                        block
+                        w-full
+                        px-3
+                        py-1.5
+                        text-base
+                        font-normal
+                        text-gray-700
+                        bg-white bg-clip-padding
+                        border border-solid border-gray-300
+                        rounded
+                        transition
+                        ease-in-out
+                        m-0
+                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                    "
+                    id="Judul"
+                    />
+                    </div>
+                </div>
+            </div>
+            <div className="Submission2">
+                <div className="mb-3 w-full inline-flex">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label inline-block mb-2 text-white ml-2"
+                    >Penjelasan:</label
+                    >
+                    <div className="w-full ml-4 mr-4 mt-2">
+                    <textarea
+                    className="
+                        form-control
+                        block
+                        w-full
+                        h-52
+                        px-3
+                        py-1.5
+                        text-base
+                        font-normal
+                        text-gray-700
+                        bg-white bg-clip-padding
+                        border border-solid border-gray-300
+                        rounded
+                        transition
+                        ease-in-out
+                        m-0
+                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                    "
+                    id="Penjelasan"
+                    ></textarea>
+                     </div>
+                </div>
+                <div className="flex mt-3 mb-5">
+                <button type="submit" className="
+                w-5/12
+                px-6
+                py-2.5
+                bg-gray-500
+                text-white
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                object-right
+                mx-32
+                ">Terima</button>
+                <button type="submit" className="
+                w-5/12
+                px-6
+                py-2.5
+                bg-gray-500
+                text-white
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                object-right
+                mx-32
+                ">Tolak</button>
+                </div>
+            </div>
+            </div>
+            </div>
+        
+        </div>
+        </div>
+
+        <div className="body">
+            <div className="flex flex-nowrap">
+
+            <div className="flex-none p-4 m-2">
+            <div className="relative">
+                
+                <div className="gap">
+                ‎ 
+                ‎ 
+                ‎ 
+                </div>
+
+                
+            </div>
+            </div>
+
+            <div className="block rounded-lg shadow-lg bg-gray-400 w-1/2 h-auto text-left mb-40">
+            <div className="flex-none">
+            <div className="Submission1">
+                <div className="mb-3 w-full inline-flex">
+                    <label htmlFor="exampleText0" className="form-label inline-block mb-2 text-white ml-2 mt-5"
+                    >Judul:</label
+                    >
+                    <div className="w-full  ml-14 mr-4 mt-4">
+                   
+                    <input
+                    type="text"
+                    className="
+                        form-control
+                        block
+                        w-full
+                        px-3
+                        py-1.5
+                        text-base
+                        font-normal
+                        text-gray-700
+                        bg-white bg-clip-padding
+                        border border-solid border-gray-300
+                        rounded
+                        transition
+                        ease-in-out
+                        m-0
+                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                    "
+                    id="Judul"
+                    />
+                    </div>
+                </div>
+            </div>
+            <div className="Submission2">
+                <div className="mb-3 w-full inline-flex">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label inline-block mb-2 text-white ml-2"
+                    >Penjelasan:</label
+                    >
+                    <div className="w-full ml-4 mr-4 mt-2">
+                    <textarea
+                    className="
+                        form-control
+                        block
+                        w-full
+                        h-52
+                        px-3
+                        py-1.5
+                        text-base
+                        font-normal
+                        text-gray-700
+                        bg-white bg-clip-padding
+                        border border-solid border-gray-300
+                        rounded
+                        transition
+                        ease-in-out
+                        m-0
+                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                    "
+                    id="Penjelasan"
+                    ></textarea>
+                     </div>
+                </div>
+                <div className="flex mt-3 mb-5">
+                <button type="submit" className="
+                w-5/12
+                px-6
+                py-2.5
+                bg-gray-500
+                text-white
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                object-right
+                mx-32
+                ">Terima</button>
+                <button type="submit" className="
+                w-5/12
+                px-6
+                py-2.5
+                bg-gray-500
+                text-white
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                object-right
+                mx-32
+                ">Tolak</button>
+                </div>
+            </div>
+            </div>
+            </div>
+        
+        </div>
+        </div>
+           
+        </>
+    )
+}
