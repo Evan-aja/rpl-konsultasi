b13b2b6881b8ba5ad5a0220fd0939bd4e2a8ac71
Evan-aja <gebel909@student.ub.ac.id>
2022-11-22 13:23:31 +0700

    Navbar + Sidebar Mahasiswa, Dosen, Admin Changes to be committed: 	modified:   components/navbar.tsx 	new file:   components/sidebar.tsx 	modified:   package-lock.json 	modified:   pages/_app.tsx 	modified:   tailwind.config.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import NavBar from '../components/navbar'
 import Footer from '../components/footer'
+import Sidebar from '../components/sidebar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NavBar/>
+      {/* <Sidebar siapa={"Mahasiswa"}/> */}
+      {/* <Sidebar siapa={"Dosen"}/> */}
+      <Sidebar siapa={"Admin"}/>
       <Component {...pageProps} />
       <Footer/>
     </>
