0681c966d6a7740f5781844528ad225d7499c29a
Evan-aja <gebel909@student.ub.ac.id>
2022-11-29 16:54:27 +0700

    backend mostly operational Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/api/get-komplain.ts 	modified:   pages/api/get-laporan.ts 	modified:   pages/respon_komplain/index.tsx 	modified:   pages/respon_laporan/index.tsx

--- pages/api/get-komplain.ts
+++ pages/api/get-komplain.ts
@@ -1,20 +1,9 @@
 import { Laporan, PrismaClient } from '@prisma/client'
-import type { NextApiRequest, NextApiResponse } from 'next'
 
-// type Data = {
-//   Datas: Laporan[]
-// }
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler() {
   const prisma=new PrismaClient()
-  const body=req.body
-  console.log('body: ',body)
   prisma.$connect
-  let result=await prisma.komplain.findMany()
+  let komplain=await prisma.komplain.findMany()
   prisma.$disconnect
-  // res.status(200).json({ Nama: body.Nama,Nip_Nim: body.Nip_Nim, Penjelasan: body.Penjelasan })
-  res.status(200).json({result})
+  return komplain
 }
--- pages/respon_laporan/index.tsx
+++ pages/respon_laporan/index.tsx
@@ -1,8 +1,30 @@
-export default function LamanResponLaporan(){
-    return(
-        <><div className="body">
-            <div className="flex flex-nowrap">
+import { Laporan } from "@prisma/client"
+import { GetServerSideProps } from "next"
+import React from "react"
+import handler from "../api/get-laporan"
 
+export function makeSerializable<T extends any> (o: T): T {
+    return JSON.parse(JSON.stringify(o))
+}
+
+type Props = {
+    laporan: Laporan[]
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const result = await handler()
+    return {
+      props: { laporan: makeSerializable(result) },
+    }
+}
+
+const ResponLaporan: React.FC<Props> = props =>{
+    return (
+        <> 
+           {props.laporan && props.laporan.map((i) => (
+            <div className="body">
+            <div className="flex flex-nowrap">
+    
             <div className="flex-none p-4 m-2">
             <div className="relative">
                 <div className="relative">
@@ -14,8 +36,9 @@ export default function LamanResponLaporan(){
                 
             </div>
             </div>
-
-            <div className="block rounded-lg shadow-lg bg-gray-400 w-1/2 h-auto text-left mb-10 mt-2">
+    
+            <form  className="block rounded-lg shadow-lg bg-gray-400 w-1/2 h-auto text-left mb-10 mt-2">
+            <input hidden readOnly placeholder={i.ID.toString()}/>
             <div className="flex-none">
             <div className="Submission1">
                 <div className="mb-3 w-full inline-flex">
@@ -42,7 +65,8 @@ export default function LamanResponLaporan(){
                         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
                     "
                     id="Nama"
-                    placeholder="Nama"
+                    name="Nama"
+                    placeholder={i.Nama?.toString()}
                     />
                     </div>
                 </div>
@@ -72,7 +96,8 @@ export default function LamanResponLaporan(){
                         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
                     "
                     id="NIP/NIM"
-                    placeholder="NIP/NIM"
+                    name="NIP/NIM"
+                    placeholder={i.NIP_NIM.toString()}
                     />
                     </div>
                 </div>
@@ -102,7 +127,8 @@ export default function LamanResponLaporan(){
                         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
                     "
                     id="Penjelasan"
-                    placeholder="Penjelasan Komplain"
+                    name="Penjelasan"
+                    placeholder={i.Penjelasan?.toString()}
                     />
                     </div>
                 </div>
@@ -142,156 +168,12 @@ export default function LamanResponLaporan(){
                 </div>
             </div>
             </div>
-            </div>
+            </form>
                     
         </div>
         </div>
-        <div className="body">
-            <div className="flex flex-nowrap">
-
-            <div className="flex-none p-4 m-2">
-            <div className="relative">
-                <div className="relative">
-                </div>
-                <div className="w-full ml-5">
-                </div>
-                <div className="relative">
-                </div>
-                
-            </div>
-            </div>
-
-            <div className="block rounded-lg shadow-lg bg-gray-400 w-1/2 h-auto text-left mb-10">
-            <div className="flex-none">
-            <div className="Submission1">
-                <div className="mb-3 w-full inline-flex">
-                    <label htmlFor="exampleText0" className="form-label inline-block mb-2 text-white ml-5 mt-6"
-                    >Nama:</label>
-                    <div className="w-full ml-10 mr-5 mt-5">
-                    <input
-                    type="text"
-                    className="
-                        form-control
-                        block
-                        w-full
-                        px-3
-                        py-1.5
-                        text-base
-                        font-normal
-                        text-gray-700
-                        bg-white bg-clip-padding
-                        border border-solid border-gray-300
-                        rounded
-                        transition
-                        ease-in-out
-                        m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                    "
-                    id="Nama"
-                    placeholder="Nama"
-                    />
-                    </div>
-                </div>
-            </div>
-            <div className="Submission2">
-                <div className="mb-3 w-full inline-flex">
-                    <label htmlFor="exampleText0" className="form-label inline-block mb-2 text-white ml-5 mt-2"
-                    >NIP/NIM:</label>
-                    <div className="w-full ml-5 mr-5">
-                    <input
-                    type="text"
-                    className="
-                        form-control
-                        block
-                        w-full
-                        px-3
-                        py-1.5
-                        text-base
-                        font-normal
-                        text-gray-700
-                        bg-white bg-clip-padding
-                        border border-solid border-gray-300
-                        rounded
-                        transition
-                        ease-in-out
-                        m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                    "
-                    id="NIP/NIM"
-                    placeholder="NIP/NIM"
-                    />
-                    </div>
-                </div>
-            </div>
-            <div className="Submission3">
-                <div className="mb-3 w-full inline-flex">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label inline-block mb-2 text-white ml-5 mt-2"
-                    >Penjelasan:</label>
-                    <div className="w-full ml-2 mr-5">
-                    <textarea
-                    className="
-                        form-control
-                        block
-                        w-full
-                        h-20
-                        px-3
-                        py-1.5
-                        text-base
-                        font-normal
-                        text-gray-700
-                        bg-white bg-clip-padding
-                        border border-solid border-gray-300
-                        rounded
-                        transition
-                        ease-in-out
-                        m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                    "
-                    id="Penjelasan"
-                    placeholder="Penjelasan Komplain"
-                    />
-                    </div>
-                </div>
-                <div className="flex mt-3 mb-5">
-                <button type="submit" className="
-                w-5/12
-                px-6
-                py-2.5
-                bg-gray-500
-                text-white
-                font-medium
-                text-xs
-                leading-tight
-                uppercase
-                object-right
-                max-mx-32
-                ml-26
-                mr-20
-                rounded-full
-                ">Terima</button>
-                <button type="submit" className="
-                w-5/12
-                px-6
-                py-2.5
-                bg-gray-500
-                text-white
-                font-medium
-                text-xs
-                leading-tight
-                uppercase
-                object-right
-                max-mx-32
-                ml-20
-                mr-5
-                rounded-full
-                ">Tolak</button>
-                </div>
-            </div>
-            </div>
-            </div>
-                    
-        </div>
-        </div>
+        ))}
         </>
     )
 }
+export default ResponLaporan
